a0cb55ffc47969bbc09fef4732cfd1d9b02dc80e
Zhaowen <1773098@qq.com>
2019-10-23 20:24:22 +0800

    新增切换城市和手动更新天气的功能。有点击城市不能刷新，必须重启才能刷新的毛病。

---
+++ app/src/main/java/com/coolweather/android/service/AutoUpdateService.java
@@ -0,0 +1,15 @@
+package com.coolweather.android.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class AutoUpdateService extends Service {
+    public AutoUpdateService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
